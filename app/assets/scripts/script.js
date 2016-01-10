1587eb49c7e9e8039a70233d30d16fe4854a0785
rinathafiatullin <rhafiyatullin@gmail.com>
2016-01-11 00:15:34 +0300

    Build sections

--- app/assets/scripts/script.js
+++ app/assets/scripts/script.js
@@ -1,9 +1,43 @@
+function buildSections(container, data) {
+    var sections = data.male.concat(data.female);
+
+    container.innerHTML = '';
+
+    sections.forEach(function (section, index) {
+        var sectionElement = document.createElement('div');
+        var sectionTitle = document.createElement('h2');
+        var sectionDesc = document.createElement('p');
+
+        sectionElement.className = 'sections__item';
+        sectionTitle.className = 'sections__title';
+        sectionDesc.className = 'sections__description';
+
+
+        sectionTitle.textContent = section.title;
+        sectionDesc.textContent = section.description;
+        sectionTitle.dataset.todoId = index + 1;
+
+        function onSectionTitleClick(e) {
+            e.target.classList.toggle("sections__title_open");
+        }
+
+        sectionTitle.addEventListener('click', onSectionTitleClick);
+
+        sectionElement.appendChild(sectionTitle);
+        sectionElement.appendChild(sectionDesc);
+        container.appendChild(sectionElement);
+    });
+
+    return container;
+}
+
 function main() {
     var data = {
         male:[],
         female:[]
     };
 
+    var container = document.querySelector('.sections');
     var input = document.querySelector('.form__input');
     var textarea = document.querySelector('.form__textarea');
     var button = document.querySelector('.form__button');
@@ -23,7 +57,7 @@ function main() {
         input.value = '';
         textarea.value = '';
 
-        console.log(data);
+        buildSections(container, data);
     });
 
 }
