295ede7c3f43d53800d90556937077b9aece71b8
Rinat Khafiyatullin <rkhafiyatullin@avito.ru>
2016-07-29 16:28:17 +0300

    centering

--- server.js
+++ server.js
@@ -5,18 +5,19 @@ var app = express();
 app.engine('html', swig.renderFile);
 
 app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+// app.set('views', __dirname + '/views');
 
 app.use('/', express.static(__dirname + '/public'));
 app.use('/', express.static(__dirname + '/app/json'));
+app.use('/', express.static(__dirname + '/views'));
 
 
 app.set('view cache', false);
 swig.setDefaults({ cache: false });
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
+// app.get('/', function (req, res) {
+//     res.render('index');
+// });
 
 app.listen(8000);
 console.log('Application Started on http://localhost:8000/');
